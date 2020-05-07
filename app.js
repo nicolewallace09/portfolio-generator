4ed1febc862b8488af140deb17d57dc00e8f7667
nicolewallace09 <nicole.elisaw@gmail.com>
2020-05-06 20:35:30 -0700

    created html

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i +=1) {
-        console.log(profileDataArr[i])
-    }
+const profileDataArgs = process.argv.slice(2);
+// console.log(profileDataArgs);
 
-    console.log('================');
+const [name, github] = profileDataArgs;
+ 
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i +=1) {
+//         console.log(profileDataArr[i])
+//     }
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+//     console.log('================');
 
-};
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
 
-printProfileData(profileDataArgs); 
+// };
 
+// printProfileData(profileDataArgs); 
 
+// arguments - file created (output file), data that is being written (html string) and error 
+fs.writeFile('./index.html', generatePage(name,github), err => {
+    if(err) throw new Error (err);
+
+    // success message 
+    console.log("Portfolio complete! Check out index.html to see the output!");
+});
