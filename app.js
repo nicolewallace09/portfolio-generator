33487cc6722953504d6865bb94de0ed9f7cd298f
nicolewallace09 <nicole.elisaw@gmail.com>
2020-05-07 14:04:28 -0700

    added first question

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require("inquirer")
+
+// const generatePage = require('./src/page-template.js');
+
+
+// const profileDataArgs = process.argv.slice(2);
 // console.log(profileDataArgs);
 
-const [name, github] = profileDataArgs;
+// const [name, github] = profileDataArgs;
+// cont pageHTML = generatePage(portfolioData);
  
 // const printProfileData = profileDataArr => {
 //     for (let i = 0; i < profileDataArr.length; i +=1) {
@@ -20,9 +25,18 @@ const [name, github] = profileDataArgs;
 // printProfileData(profileDataArgs); 
 
 // arguments - file created (output file), data that is being written (html string) and error 
-fs.writeFile('./index.html', generatePage(name,github), err => {
-    if(err) throw new Error (err);
+// fs.writeFile('./index.html', generatePage(name,github), err => {
+//     if(err) throw new Error (err);
 
-    // success message 
-    console.log("Portfolio complete! Check out index.html to see the output!");
-});
+//     // success message 
+//     console.log("Portfolio complete! Check out index.html to see the output!");
+// });
+
+inquirer.prompt ([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
